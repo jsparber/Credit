5c0832a5c1d36f6e406be38b0fd05eacf56aba71
Julian Sparber <julian@sparber.net>
2015-03-01 19:57:50 +0100

    [doc] add transform reactify

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ Frontend for a the Credit app witch i will write sooner or later.
 ###Build
 		npm install
 		sudo npm -g install browserify
-		browserify app.js > js/main.js
+		browserify -t reactify app.js > js/main.js
--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 var React = require('react');
+
+var Content = React.createClass({
+	render: function() {
+		console.log(this);
+		return(
+				<section id="content">
+				Hello World
+				</section>
+				);
+	}
+});
+
+React.render(
+		<Content/>,
+		document.getElementById("container")
+		);
